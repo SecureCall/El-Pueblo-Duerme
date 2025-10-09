7cfe830502012500096fbbbd32c8692a520b806f
zalena tarot <zalenatarot@gmail.com>
2025-10-09 23:55:47 +0000

    ya te eh metido la imagen de doctor.png  que faltaba

--- src/components/game/CreateGameForm.tsx
+++ src/components/game/CreateGameForm.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { FlaskConical, Crown, Fingerprint, Users2, Loader2, HelpCircle, Heart, Shield, Sparkles, User, Crosshair, BotIcon } from "lucide-react";
+import { FlaskConical, Crown, Fingerprint, Users2, Loader2, HelpCircle, Heart, Shield, Sparkles, User, Crosshair, BotIcon, BriefcaseMedical } from "lucide-react";
 
 
 import { useGameSession } from "@/hooks/use-game-session";
@@ -53,7 +53,7 @@ const FormSchema = z.object({
 
 const specialRoles = [
   { id: 'seer', label: 'Vidente', Icon: Fingerprint, description: 'Descubre el rol de un jugador cada noche.' },
-  { id: 'doctor', label: 'Doctor', Icon: Crosshair, description: 'Protege a un jugador del ataque de los lobos. No puede curar a la misma persona 2 noches seguidas.' },
+  { id: 'doctor', label: 'Doctor', Icon: BriefcaseMedical, description: 'Protege a un jugador del ataque de los lobos. No puede curar a la misma persona 2 noches seguidas.' },
   { id: 'hechicera', label: 'Hechicera', Icon: FlaskConical, description: 'Usa una poción de vida y una de muerte.' },
   { id: 'hunter', label: 'Cazador', Icon: Crosshair, description: 'Al morir, puede llevarse a otro jugador consigo.' },
   { id: 'prince', label: 'Príncipe', Icon: Crown, description: 'Inmune a ser linchado por votación.' },
