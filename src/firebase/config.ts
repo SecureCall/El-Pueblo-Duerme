c61225d678879f0f0f26df4d99d031841863a698
zalena tarot <zalenatarot@gmail.com>
2025-10-10 11:52:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9015007721-ac536",
+  "appId": "1:1064367864733:web:fcae731c7f8880f2b28d74",
+  "apiKey": "AIzaSyCl3guWzQNlp7KasDyzIAAu6RPCG03Mw-U",
+  "authDomain": "studio-9015007721-ac536.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1064367864733"
+};
