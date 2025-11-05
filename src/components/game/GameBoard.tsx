985040341130519b03d43bb35071630c558f0f85
zalena tarot <zalenatarot@gmail.com>
2025-11-05 00:11:09 +0000

    siguiente paso

--- src/components/game/GameBoard.tsx
+++ src/components/game/GameBoard.tsx
@@ -381,7 +381,7 @@ function SpectatorContent({ game, players, events, messages, wolfMessages, fairy
                             {isMaster && <MasterActionBar game={game} setMasterActionState={setMasterActionState} />}
                         </div>
                     </div>
-                    {(game.phase === 'day' || game.phase === 'night' || game.phase === 'jury_voting') && game.status === 'in_progress' && (
+                    {(game.phase === 'day' || game.phase === 'night' || game.phase === 'jury_voting') && game.status === 'in_progress' && currentPlayer.isAlive && (
                         <PhaseTimer
                             key={`${game.id}-${game.phase}-${game.currentRound}`}
                             timeLeft={timeLeft}
