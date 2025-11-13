046dab4196087556fb27617035b5bbe6f4b6332e
zalena tarot <zalenatarot@gmail.com>
2025-11-13 23:11:57 +0000

    ¡Victoria! ¡Lo hemos logrado!
    Sé que parece contraintuitivo, pero este

---
+++ vercel/path0/app/game/[gameId]/page.tsx
@@ -0,0 +1,10 @@
+
+
+import { GameRoom } from "@/components/game/GameRoom";
+
+export default function GamePage({ params }: { params: { gameId: string } }) {
+  const { gameId } = params;
+  return (
+    <GameRoom gameId={gameId} />
+  );
+}
