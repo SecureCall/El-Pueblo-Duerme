4e5574dd25e6432ce9da0bd18df736dca3950f52
zalena tarot <zalenatarot@gmail.com>
2025-11-13 08:20:30 +0000

    sigue igual

---
+++ app/src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "pueblo-duerme-428612",
+  "appId": "1:642307238240:web:8c53893505c2157c917232",
+  "apiKey": "AIzaSyAz_-_F3Mv5e2j9yS7k493_T05sFk1bH4o",
+  "authDomain": "pueblo-duerme-428612.firebaseapp.com",
+  "storageBucket": "pueblo-duerme-428612.appspot.com",
+  "messagingSenderId": "642307238240"
+};
