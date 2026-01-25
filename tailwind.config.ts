aec7e3183c1e968c51b89bb41f28cc2fff231b1c
zalena tarot <zalenatarot@gmail.com>
2026-01-25 23:20:55 +0000

    75% Funcional Diagnóstico de Salud Altamente Funcional
    Nivel Actual
    Met

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
-
 import type { Config } from "tailwindcss"
  
 const config = {
   darkMode: ["class"],
   content: [
     "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
     "./lib/**/*.{ts,tsx}",
 	],
   prefix: "",
