a4a689fa50a724aa4373df51ad57600904eb121f
zalena tarot <zalenatarot@gmail.com>
2026-01-28 23:20:04 +0000

    Set up a Firebase backend

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -25,6 +25,18 @@ export interface UseCollectionResult<T> {
   error: FirestoreError | Error | null; // Error object, or null.
 }
 
+/* Internal implementation of Query:
+  https://github.com/firebase/firebase-js-sdk/blob/c5f08a9bc5da0d2b0207802c972d53724ccef055/packages/firestore/src/lite-api/reference.ts#L143
+*/
+export interface InternalQuery extends Query<DocumentData> {
+  _query: {
+    path: {
+      canonicalString(): string;
+      toString(): string;
+    }
+  }
+}
+
 /**
  * React hook to subscribe to a Firestore collection or query in real-time.
  * Handles nullable references/queries.
@@ -73,18 +85,11 @@ export function useCollection<T = any>(
         setIsLoading(false);
       },
       (error: FirestoreError) => {
-        let path = 'unknown/path';
-        try {
-            if (memoizedTargetRefOrQuery.type === 'collection') {
-                path = (memoizedTargetRefOrQuery as CollectionReference).path;
-            } else if (memoizedTargetRefOrQuery.type === 'query') {
-                 // The 'query' type gives us access to the underlying collection reference path.
-                 // This is a much safer way to get the path than accessing internal properties.
-                 path = (memoizedTargetRefOrQuery as Query)._query.path.toString()
-            }
-        } catch (e) {
-            console.warn("Could not determine path for Firestore permission error reporting.", e);
-        }
+        // This logic extracts the path from either a ref or a query
+        const path: string =
+          memoizedTargetRefOrQuery.type === 'collection'
+            ? (memoizedTargetRefOrQuery as CollectionReference).path
+            : (memoizedTargetRefOrQuery as unknown as InternalQuery)._query.path.canonicalString()
 
         const contextualError = new FirestorePermissionError({
           operation: 'list',
