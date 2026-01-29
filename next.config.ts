7e414e1d19a101b6d6e8da2cb7637f1a79493dbc
zalena tarot <zalenatarot@gmail.com>
2026-01-29 10:33:05 +0000

    npm run build
    > el-pueblo-duerme@0.1.0 build
    > next build
    
      ▲ Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config) => {
+        // This is to suppress a benign warning from a dependency of Genkit (OpenTelemetry).
+        // The 'require-in-the-middle' package uses dynamic requires that Webpack can't statically analyze.
+        config.ignoreWarnings = [
+            ...(config.ignoreWarnings || []),
+            { module: /require-in-the-middle/ }
+        ];
+        return config;
+    },
 };
 
 module.exports = nextConfig;
