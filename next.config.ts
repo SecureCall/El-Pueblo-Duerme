8cd283bc450df53fd6ef6b5ff5a1a395d94633b9
zalena tarot <zalenatarot@gmail.com>
2026-01-29 10:36:18 +0000

    npm run build
    > el-pueblo-duerme@0.1.0 build
    > next build
    
      ▲ Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
+import type { NextConfig } from 'next';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
     reactStrictMode: true,
     webpack: (config) => {
         // This is to suppress a benign warning from a dependency of Genkit (OpenTelemetry).
@@ -12,4 +14,4 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
