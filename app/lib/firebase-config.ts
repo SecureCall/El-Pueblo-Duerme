2f7f33886644b250e0487ef50ff23d28d9f23630
Your Name <zalenatarot@gmail.com>
2026-02-03 18:10:47 +0000

    File changes

--- app/lib/firebase-config.ts
+++ app/lib/firebase-config.ts
@@ -1,10 +1,10 @@
 // This file is safe to be public on the client-side.
 // It does not contain any sensitive credentials.
 export const firebaseConfig = {
-  apiKey: "AIzaSyCl3guWzQNLp7KasDyZIAu6RPCG0Mv-U",
-  authDomain: "studio-9015007721-ac536.firebaseapp.com",
-  projectId: "studio-9015007721-ac536",
-  storageBucket: "studio-9015007721-ac536.firebasestorage.app",
-  messagingSenderId: "1064367864733",
-  appId: "1:1064367864733:web:19aa83d24a7fb388b28d74"
+  "projectId": "studio-9015007721-ac536",
+  "appId": "1:1064367864733:web:fcae731c7f8880f2b28d74",
+  "apiKey": "AIzaSyCl3guWzQNlp7KasDyzIAAu6RPCG03Mw-U",
+  "authDomain": "studio-9015007721-ac536.firebaseapp.com",
+  "storageBucket": "studio-9015007721-ac536.appspot.com",
+  "messagingSenderId": "1064367864733"
 };
