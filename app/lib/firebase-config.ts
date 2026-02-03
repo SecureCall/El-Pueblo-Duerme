1f3a3cdc64d3057c03583fb3b6d9a35395d3d27e
zalena tarot <zalenatarot@gmail.com>
2026-02-03 01:19:55 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCl3guWzQNlp7KasDyzIAAu6RPCG03M

--- app/lib/firebase-config.ts
+++ app/lib/firebase-config.ts
@@ -5,7 +5,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCl3guWzQNlp7KasDyzIAAu6RPCG03Mw-U",
   authDomain: "studio-9015007721-ac536.firebaseapp.com",
   projectId: "studio-9015007721-ac536",
-  storageBucket: "studio-9015007721-ac536.appspot.com",
+  storageBucket: "studio-9015007721-ac536.firebasestorage.app",
   messagingSenderId: "1064367864733",
-  appId: "1:1064367864733:web:fcae731c7f8880f2b28d74"
+  appId: "1:1064367864733:web:19aa83d24a7fb388b28d74"
 };
